42289fdd0dc36cdd5784a0faede3a81389d7780b
tstellfe <tstellfe@icloud.com>
2021-12-02 18:46:43 +0100

    day01 part2

--- src/Day01/Day01.kt
+++ src/Day01/Day01.kt
@@ -1,18 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        var countIncreases = 0
-        input.map { it.toInt() }.reduce { first, second ->
-            if (second > first) countIncreases++
-            second
-        }
-        return countIncreases
+  fun part1(input: List<Int>): Int {
+    var countIncreases = 0
+    input.reduce { first, second ->
+      if (second > first) countIncreases++
+      second
+    }
+    return countIncreases
+  }
+
+  fun part2(input: List<Int>): Int {
+    val summedInput = input.windowed(3, 1, true) {
+      it.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    return part1(summedInput)
+  }
 
-    val testInput = readInput("Day01", "Day01")
-    println(part1(testInput))
-
+  val testInput = readInput("Day01", "Day01").map { it.toInt() }
+  println(part2(testInput))
 }
