33ba2dd32b4396c76720aa6dfba79b8350ce02af
tstellfe <tstellfe@icloud.com>
2021-12-02 19:42:55 +0100

    refactor to keep solution day2 part1

--- src/Day02.kt
+++ src/Day02.kt
@@ -4,14 +4,20 @@ fun main() {
   var aim = 0
 
   fun part1(input: List<String>): Int {
-    input.map {
-      it.split(" ").let { (direction, value) ->
-        Pair(direction, +value.toInt())
-      }
-    }.forEach {
+    input.mapInputToDirectionPair().forEach {
+      if (it.first == "forward") horizontal += it.second
+      if (it.first == "down") depth += it.second
+      if (it.first == "up") depth -= it.second
+    }
+
+    return horizontal * depth
+  }
+
+  fun part2(input: List<String>): Int {
+    input.mapInputToDirectionPair().forEach {
       if (it.first == "forward") {
         horizontal += it.second
-        if (aim != 0) depth += aim*it.second
+        if (aim != 0) depth += aim * it.second
       }
       if (it.first == "down") aim += it.second
       if (it.first == "up") aim -= it.second
@@ -20,10 +26,12 @@ fun main() {
     return horizontal * depth
   }
 
-  fun part2(input: List<String>): Int {
-    return 0
-  }
-
   val testInput = readInput("Day02")
-  println(part1(testInput))
+  println(part2(testInput))
+}
+
+fun List<String>.mapInputToDirectionPair(): List<Pair<String, Int>> = this.map {
+  it.split(" ").let { (direction, value) ->
+    Pair(direction, +value.toInt())
+  }
 }
