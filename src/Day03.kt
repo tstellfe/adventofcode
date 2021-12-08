def4d2cf8cd03cf9ba7d46025f53b08eab9857f8
tstellfe <tstellfe@icloud.com>
2021-12-08 09:13:58 +0100

    day03 part2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,33 +1,62 @@
-import java.io.File
-import java.util.BitSet
-
 fun main() {
   fun part1(input: List<String>): Int {
     val columnMap = input.switchLinesToColumns()
-    val epsilonRate = columnMap.calculateRate('0', '1')
-    val gammaRate = columnMap.calculateRate('1', '0')
+    val epsilonRate = columnMap.calculateRate(0, 1)
+    val gammaRate = columnMap.calculateRate(1, 0)
 
-    return gammaRate*epsilonRate
+    return gammaRate * epsilonRate
   }
 
   fun part2(input: List<String>): Int {
-    return 0
+    val columnMap = input.switchLinesToColumns()
+    val oxygenGeneratorRating = columnMap.calculateRating(1, 0)
+    println(oxygenGeneratorRating)
+    val co2ScrubberRating = columnMap.calculateRating(0, 1)
+    println(co2ScrubberRating)
+    return oxygenGeneratorRating*co2ScrubberRating
   }
 
   val testInput = readInput("Day03");
-  println(part1(testInput))
+  println(part2(testInput))
 }
 
-fun List<String>.switchLinesToColumns(): MutableMap<Int, MutableList<Char>> {
-  val map = mutableMapOf<Int, MutableList<Char>>()
+fun List<String>.switchLinesToColumns(): List<MutableList<Int>> {
+  val charList = mutableListOf<MutableList<Int>>()
   this.map { line ->
     line.toCharArray().forEachIndexed { index, bit ->
-      map[index]?.add(bit) ?: map.putIfAbsent(index, mutableListOf(bit))
+      charList.getOrNull(index)?.add(bit.digitToInt()) ?: charList.add(index, mutableListOf(bit.digitToInt()))
     }
   }
-  return map
+  return charList
 }
 
-fun MutableMap<Int, MutableList<Char>>.calculateRate(small: Char, big: Char) = this.map { item ->
-  if (item.value.count { it == big } > item.value.count { it == small }) 0 else 1
-}.joinToString("", "").toInt(2)
+fun List<MutableList<Int>>.calculateRate(small: Int, big: Int) = this.map { item ->
+  if (item.count { it == big } > item.count { it == small }) 0 else 1
+}.toDec()
+
+/**
+ * don't even look at it...
+ */
+fun List<MutableList<Int>>.calculateRating(small: Int, big: Int): Int {
+  val indexes = mutableListOf<Int>()
+  return this.map { item ->
+    val filtered = item.filterIndexed { i, _ -> !indexes.contains(i) }
+
+    if (filtered.size == 1) {
+      return this.map { it.filterIndexed { i, _ -> !indexes.contains(i) }.first() }.toDec()
+    }
+
+    val keepBit = if (filtered.count { it == 0 } > filtered.count { it == 1 }) big else small
+
+    item.filterIndexed { i, bit ->
+      if (bit != keepBit) {
+        indexes.add(i)
+      }
+      false
+    }
+
+    keepBit
+  }.toDec()
+}
+
+fun List<Int>.toDec() = this.joinToString("", "").toInt(2)
