5b4b327376f9e7b05efd400e6feac2d97650a080
tstellfe <tstellfe@icloud.com>
2021-12-10 16:19:44 +0100

    day05 part2

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,7 +1,16 @@
+import org.w3c.dom.ranges.Range
+
 data class Coordinate(val x: Int, val y: Int)
 
 fun main() {
-  fun createCoordinateRange(from: Coordinate, to: Coordinate): List<Coordinate> {
+  fun createCoordinateRangeDiagonal(from: Coordinate, to: Coordinate): List<Coordinate> {
+    val horizontal = if (from.x < to.x) from.x..to.x else from.x downTo to.x
+    val vertical = if (from.y < to.y) from.y..to.y else from.y downTo to.y
+
+    return horizontal.mapIndexed { i, x -> Coordinate(x, vertical.elementAt(i)) }
+  }
+
+  fun createCoordinateRange(from: Coordinate, to: Coordinate, withDiagonal: Boolean = false): List<Coordinate> {
     val result = mutableListOf<Coordinate>()
     if (from.x == to.x) {
       if (from.y < to.y) for (y in from.y..to.y) result.add(Coordinate(from.x, y))
@@ -9,6 +18,8 @@ fun main() {
     } else if (from.y == to.y) {
       if (from.x < to.x) for (x in from.x..to.x) result.add(Coordinate(x, from.y))
       else for (x in to.x..from.x) result.add(Coordinate(x, from.y))
+    } else if(withDiagonal) {
+      return createCoordinateRangeDiagonal(from, to)
     }
     return result
   }
@@ -22,11 +33,15 @@ fun main() {
   }
 
   fun part2(input: List<String>): Int {
-    return 0
+    val results = input.toCoordinatePairs().map { (from, to) ->
+      createCoordinateRange(from, to, true)
+    }.flatten()
+
+    return results.groupBy { it }.filter { it.value.size > 1 }.size
   }
 
   val testInput = readInput("Day05")
-  println(part1(testInput))
+  println(part2(testInput))
 }
 
 fun List<String>.toCoordinatePairs(): List<Pair<Coordinate, Coordinate>> =
