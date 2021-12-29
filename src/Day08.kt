29d57e5554d7009bd6eeaa6a014223e83bd2739c
tstellfe <tstellfe@icloud.com>
2021-12-29 13:11:11 +0100

    day8 part1

--- src/Day08.kt
+++ src/Day08.kt
@@ -7,6 +7,19 @@ fun main() {
     8 to 7
   )
 
+  val digitMap = mapOf(
+    0 to 6,
+    1 to 2,
+    2 to 5,
+    3 to 5,
+    4 to 4,
+    5 to 5,
+    6 to 6,
+    7 to 3,
+    8 to 7,
+    9 to 6
+  )
+
   fun part1(input: List<String>): Int {
     return input.sumOf { line ->
       line.toSegmentDigits().second.mapNotNull { d ->
@@ -16,12 +29,16 @@ fun main() {
   }
 
   fun part2(input: List<String>): Int {
+    input.forEach { line ->
+      line.toSegmentDigits().first
+    }
+
     return 0
   }
 
-  val testInput = readInput("Day08")
+  val testInput = readInput("Day08_test")
 
-  println(part1(testInput))
+  println(part2(testInput))
 }
 
 fun String.toSegmentDigits(): Pair<List<String>, List<String>> {
